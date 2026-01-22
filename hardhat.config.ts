80d79702b77a87e403816f4e49216a6f2c0a10ea
zvidza <tadiwamandizvidza@gmail.com>
2026-01-22 23:59:43 +0000

    Initial Setup (Hardhat and openzeppelin) part idk

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;
